fcea681a84f0026d87b4504b1264facb55f1f3cc
chautran <48544755+tranchausky@users.noreply.github.com>
2025-07-25 11:26:30 +0700

    Update my-script.js

--- assets/js/my-script.js
+++ assets/js/my-script.js
@@ -1,34 +1,28 @@
-
   let posts = null;
-
   document.getElementById('search-input').addEventListener('input', async function () {
     const query = this.value.toLowerCase().trim();
     const resultsContainer = document.getElementById('search-results');
-
     if (!posts) {
       try {
         const res = await fetch('/search.json');
         posts = await res.json();
       } catch (err) {
-        console.error('Không thể tải search.json:', err);
+        console.error('Cannot get search.json:', err);
         return;
       }
     }
 
-    // Xóa kết quả cũ
     resultsContainer.innerHTML = '';
 
     if (query === '') return;
 
-    // Lọc bài viết khớp tiêu đề
     const matched = posts.filter(post =>
         post.title.toLowerCase().includes(query) ||
         post.url.toLowerCase().includes(query)
     );
 
-    // Hiển thị kết quả
     if (matched.length === 0) {
-      resultsContainer.innerHTML = '<li>Không tìm thấy bài viết.</li>';
+      resultsContainer.innerHTML = '<li>Not found.</li>';
     } else {
       matched.forEach(post => {
         const li = document.createElement('li');
